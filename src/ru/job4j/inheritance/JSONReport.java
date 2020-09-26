b865fbbc4da47166937b1c9e55e66bfb2881d1c0
SalavatM <techprosm@yandex.ru>
2020-09-26 22:16:55 +0300

    5. Аннотация @Override [#357887]

--- src/ru/job4j/inheritance/JSONReport.java
+++ src/ru/job4j/inheritance/JSONReport.java
@@ -1,7 +1,8 @@
 package ru.job4j.inheritance;
 
-public class JSONReport {
-    public String generate(String name, String body) {
+public class JSONReport extends TextReport {
+    @Override
+    public String generate(String name, String  body) {
         return "name : " + name + System.lineSeparator() + "body : " + body;
     }
 }
