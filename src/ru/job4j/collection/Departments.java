aeacb626502ed59951e931e593f53ebe66548a31
SalavatM <techprosm@yandex.ru>
2020-11-02 22:09:52 +0300

    2. Отсортировать департаменты [#357790]

---
+++ src/ru/job4j/collection/Departments.java
@@ -0,0 +1,31 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class Departments {
+    public static List<String> fillGaps(List<String> deps) {
+        HashSet<String> tmp = new HashSet<>();
+        for (String value : deps) {
+            String start = "";
+            for (String el : value.split("/")) {
+                if (start.equals("")) {
+                    start = el;
+                } else {
+                    start = start + "/" + el;
+                }
+                tmp.add(start);
+            }
+        }
+        List<String> rsl = new ArrayList<>(tmp);
+        sortAsc(rsl);
+        return rsl;
+    }
+
+    public static void sortAsc(List<String> orgs) {
+        orgs.sort(Comparator.naturalOrder());
+    }
+
+    public static void sortDesc(List<String> orgs) {
+        orgs.sort(new DepDescComp());
+    }
+}
