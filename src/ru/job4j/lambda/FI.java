2b06ed1f25c883884d80ff08830c0c771ded94ab
SalavatM <techprosm@yandex.ru>
2020-11-04 19:27:41 +0300

    2.1. Лямбда [#357623]

---
+++ src/ru/job4j/lambda/FI.java
@@ -0,0 +1,34 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class FI {
+    public static void main(String[] args) {
+        Attachment[] atts = {
+                new Attachment("image 1", 20),
+                new Attachment("image 3", 120),
+                new Attachment("image 2", 23)
+        };
+        //сортировать по полю size
+        Comparator<Attachment> comparator = new Comparator<>() {
+            @Override
+            public int compare(Attachment left, Attachment right) {
+                return left.getSize() - right.getSize();
+            }
+        };
+        Arrays.sort(atts, comparator);
+        System.out.println(Arrays.toString(atts));
+
+        //сортировать строки по имени
+        Comparator<Attachment> comparator2 = (left, right) -> (left.getName().compareTo(right.getName()));
+        //Comparator<Attachment> comparator2 = Comparator.comparing(Attachment::getName);
+        Arrays.sort(atts, comparator2);
+        System.out.println(Arrays.asList(atts));
+
+        //сортировать строки по убыванию длины
+        Comparator<Attachment> comparator3 = (left, right) -> (right.getSize() - left.getSize());
+        Arrays.sort(atts, comparator3);
+        System.out.println(Arrays.asList(atts));
+    }
+}
